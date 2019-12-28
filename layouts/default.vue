0bd72c08835bf85a29179acff8dbe74a828a3a2f
Lchushan <994101200@qq.com>
2019-12-28 16:10:03 +0800

    完成项目的页头和页脚的公共样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,110 @@
 <template>
   <div>
+    <Header />
+    <!-- 相当于router-view，占位符 -->
     <nuxt />
+    <Footer />
   </div>
 </template>
 
+
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+font,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+dd,
+dl,
+dt,
+li,
+ol,
+ul,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td {
   margin: 0;
+  padding: 0;
+  border: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+ol,
+ul {
+  list-style: none;
+}
+q:before,
+q:after,
+blockquote:before,
+blockquote:after {
+  content: '';
+}
+a {
+  color: inherit;
   text-decoration: none;
-  padding: 10px 30px;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+a:hover {
+  color: inherit;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+i,
+em {
+  font-style: normal;
 }
 </style>
---
+++ pages/tab.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="tab">
+    <div class="head">
+      <span
+        v-for="(item,index) in ['新闻','体育','娱乐']"
+        :key="index"
+        @click="handlerClick(index)"
+        :class="{active:current===index}"
+      >{{item}}</span>
+    </div>
+    <div class="body">
+      <div v-if="current===0">
+        <ul>
+          <li>普通新闻1</li>
+          <li>普通新闻2</li>
+          <li>普通新闻3</li>
+        </ul>
+      </div>
+      <div v-if="current===1">
+        <ul>
+          <li>体育新闻1</li>
+          <li>体育新闻2</li>
+          <li>体育新闻3</li>
+        </ul>
+      </div>
+      <div v-if="current===2">
+        <ul>
+          <li>娱乐新闻1</li>
+          <li>娱乐新闻2</li>
+          <li>娱乐新闻3</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: 0
+    }
+  },
+  methods: {
+    handlerClick(index) {
+      this.current = index
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.tab {
+  margin: 100px auto;
+  width: 600px;
+  height: 500px;
+  border: 1px solid #000;
+  .head {
+    span.active {
+      color: #f00;
+      border-bottom: 1px solid #f00;
+    }
+  }
+  .body {
+    position: relative;
+    > div {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+}
+</style>
