d3d0a7c6d33f9e86d17b3f0b7cb963a79572c48f
Lchushan <994101200@qq.com>
2019-12-31 15:30:00 +0800

    优化注册，添加二次验证，封装axios获取验证码请求和注册请求

--- components/user/register.vue
+++ components/user/register.vue
@@ -13,7 +13,7 @@
     </el-form-item>
 
     <el-form-item class="form-item" prop="nickname">
-      <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
+      <el-input placeholder="你的昵称" v-model="form.nickname"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item" prop="password">
@@ -33,13 +33,18 @@ export default {
   data() {
     // 用户名的验证
     const validateUsername = (rule, value, callback) => {
+      // 手机号码的11位数字的规则
       const reg = /^1[3-9][0-9]{9}$/
-      if (value === '') {
+      if (value.trim() === '') {
+        //不能为空
         callback('请输入用户名')
       } else {
+        // 判断是否符合手机号的规则
         if (reg.test(value)) {
+          // 符合
           callback()
         } else {
+          //不符合
           callback('手机号码格式错误')
         }
       }
@@ -47,7 +52,7 @@ export default {
     // 第一次的密码验证
     const validatePassword = (rule, value, callback) => {
       // 判断第一次密码是否为空
-      if (value === '') {
+      if (value.trim() === '') {
         callback(new Error('请输入密码'))
       } else {
         //如果第二次密码不为空，校验第二次的密码
@@ -61,7 +66,7 @@ export default {
     // 第二次的密码验证（确定两次密码是否相同）
     const validateCheckPassword = (rule, value, callback) => {
       //判断第二次密码是否为空
-      if (value === '') {
+      if (value.trim() === '') {
         callback(new Error('请再次输入密码'))
       } else if (value !== this.form.password) {
         //判断两次密码是否一致
@@ -103,22 +108,36 @@ export default {
       let rule = /^1[3-9][0-9]{9}$/
       if (rule.test(this.form.username)) {
         //发送请求获取手机验证码
-        this.$axios({
-          url: '/captchas',
-          method: 'post',
-          data: {
-            tel: this.form.username
-          }
-        }).then(res => {
-          //成功获取到验证码
-          this.$message.success('验证码：' + res.data.code)
-        })
+        this.$store
+          .dispatch('user/sendCaptcha', this.form.username)
+          .then(res => {
+            //成功获取到验证码
+            this.$message.success('验证码：' + res.data.code)
+          })
       }
     },
 
     // 注册
     handleRegSubmit() {
-      console.log(this.form)
+      // 点击注册按钮在进行一次验证
+      this.$refs.form.validate(valid => {
+        // 验证成功
+        if (valid) {
+          // 获取发送注册请求需要的4个重要参数 username,nickname,captcha,password === props
+          const { checkPassword, ...props } = this.form
+          console.log(props)
+          // 调用state中的注册请求
+          this.$store.dispatch('user/register', props).then(res => {
+            if (res) {
+              //注册成功，提示注册成功
+              this.$message.success('注册成功')
+            }
+          })
+        } else {
+          // 二次验证不通过的时候，提示用户填写完整的信息
+          this.$message.error('请填写完整表单的信息')
+        }
+      })
     }
   }
 }
--- store/user.js
+++ store/user.js
@@ -1,5 +1,6 @@
 // 保存user下的数据
 export const state = {
+  // 登录用户的信息
   userInfo: ''
 }
 
@@ -8,6 +9,7 @@ export const state = {
 export const mutations = {
   //自定义一个函数，修改name属性
   // mutations函数的第一和参数必须是state，state就是上面的state对象
+  // 同步修改登录用户信息
   setUserInfo(state, data) {
     // 函数的修改方式是为了方便修改的其他逻辑
     state.userInfo = data
@@ -17,10 +19,36 @@ export const mutations = {
 
 // 异步修改state的数据
 export const actions = {
+  // 1. 发送登录请求
   login(state, data) {
     //登录接口提交，必须需要return，返回值
     return this.$axios({
       url: '/accounts/login',
+      method: 'post',
+      data
+    }).then(res => {
+      //登录成功返回上一页面
+      this.$router.back()
+      //把数据储存在state中
+      state.commit('setUserInfo', res.data)
+      // 返回true
+      return true
+    })
+  },
+  // 2. 发送验证码请求
+  sendCaptcha(state, data) {
+    return this.$axios({
+      url: '/captchas',
+      method: 'post',
+      data: {
+        tel: data
+      }
+    })
+  },
+  // 3. 发送注册请求,需要注册成功之后，直接登录
+  register(state, data) {
+    return this.$axios({
+      url: '/accounts/register',
       method: 'post',
       data
     }).then(res => {
