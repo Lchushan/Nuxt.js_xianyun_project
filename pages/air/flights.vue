dd09304594d3a2c509a1d361b2aead2d38ceb151
Lchushan <994101200@qq.com>
2020-01-02 21:00:40 +0800

    机票列表页的分页页码

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -10,7 +10,18 @@
         <FlightsListHead />
 
         <!-- 航班信息 -->
-        <FlightsItem :data="item" v-for="(item,index) in flightsData.flights" :key="index"  />
+        <FlightsItem :data="item" v-for="(item,index) in dataList" :key="index" />
+
+        <!-- 分页页码 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
       </div>
 
       <!-- 侧边栏 -->
@@ -28,14 +39,37 @@ import FlightsItem from '@/components/air/flightsItem'
 export default {
   data() {
     return {
+      // 航班总数据 { info, flights, total, options }
       flightsData: {
-        flights:{}
-      }
+        flights: []
+      },
+      // 缓存的变量，当该变量一旦被赋值之后不会被修改
+      cacheFlightsData: {
+        flights: []
+      },
+      // 当前的页面
+      pageIndex: 1,
+      // 当然的条数
+      pageSize: 5,
+      // 总条数
+      total: 0
     }
   },
   components: {
     FlightsListHead,
     FlightsItem
+  },
+  computed: {
+    // 计算属性监听函数内部引用实例的属性变化，一旦发生了变化，该函数会重新计算，并且返回新的值
+    dataList() {
+      // 请求如果还没完成，返回空数组
+      if (!this.flightsData.flights) return []
+      // 计算分页的数据
+      return this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      )
+    }
   },
   mounted() {
     // 发送获取机票列表请求
@@ -43,8 +77,24 @@ export default {
       url: '/airs',
       params: this.$route.query
     }).then(res => {
+      // 将数据赋值flightsData
       this.flightsData = res.data
+      // console.log(this.flightsData.flights)
+      // 这个是缓存的变量，一旦赋值之后不能被改
+      this.cacheFlightsData = { ...res.data }
+      // 获取总条数total
+      this.total = this.flightsData.total
     })
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.pageSize = val
+      // console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      this.pageIndex = val
+      // console.log(`当前页: ${val}`)
+    }
   }
 }
 </script>
