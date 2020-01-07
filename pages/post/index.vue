d56d13ff360412a03fbb7eb6b189a88ce47f9033
Lchushan <994101200@qq.com>
2020-01-07 13:56:12 +0800

    旅游的左侧的导航静态页面

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,175 @@
 <template>
-  <div>攻略首页</div>
+  <!-- <div class="container"> -->
+  <div class="container">
+    <div class="layout">
+      <!-- 左侧栏 -->
+      <div width="200px" class="aside">
+        <div class="menus-body">
+          <!-- 主体 -->
+          <div class="menus">
+            <div class="menus-item" @mouseenter="isShow=true" @mouseleave="isShow=false">
+              热门城市
+              <i class="el-icon-arrow-right"></i>
+            </div>
+            <div class="menus-item">
+              推荐城市
+              <i class="el-icon-arrow-right"></i>
+            </div>
+            <div class="menus-item">
+              奔向海岛
+              <i class="el-icon-arrow-right"></i>
+            </div>
+            <div class="menus-item">
+              主体推荐
+              <i class="el-icon-arrow-right"></i>
+            </div>
+          </div>
+          <!-- 浮动布局 -->
+          <div class="sub-munes" v-if="isShow">
+            <ul>
+              <li class="sub-munes-item">
+                <a href="#">
+                  <i>1</i>
+                  <strong>北京</strong>
+                  <span>世界著名古都和现代化国际城市</span>
+                </a>
+              </li>
+              <li class="sub-munes-item">
+                <a href="#">
+                  <i>2</i>
+                  <strong>广州</strong>
+                  <span>粤澳港大湾区/反珠江三角洲经济区</span>
+                </a>
+              </li>
+              <li class="sub-munes-item">
+                <a href="#">
+                  <i>3</i>
+                  <strong>上海</strong>
+                  <span>长江入海口，东隔东中国海</span>
+                </a>
+              </li>
+              <li class="sub-munes-item">
+                <a href="#">
+                  <i>4</i>
+                  <strong>成都</strong>
+                  <span>国家历史文化名称</span>
+                </a>
+              </li>
+              <li class="sub-munes-item">
+                <a href="#">
+                  <i>5</i>
+                  <strong>西安</strong>
+                  <span>中国国际形象最佳城市之一</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- 主体 -->
+      <div class="main">Main</div>
+    </div>
+  </div>
+  <!-- </div> -->
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      isShow: false
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container {
+  margin: 0 auto;
+  padding: 30px 0;
+  width: 1000px;
+  height: 1000px;
+  .layout {
+    position: relative;
+    height: 100%;
+    background-color: gold;
+    .aside {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 260px !important;
+      .menus-body {
+        position: relative;
+        height: 165px;
+        background-color: green;
+        .menus {
+          height: 164px;
+          background-color: #fff;
+          border-left: 1px solid #ddd;
+          border-top: 1px solid #ddd;
+          .menus-item {
+            position: relative;
+            padding: 0 20px;
+            height: 40px;
+            line-height: 40px;
+            border-right: 1px solid #ddd;
+            border-bottom: 1px solid #ddd;
+            z-index: 3;
+            &:hover {
+              border-right-color: #fff;
+              color: #ffa500;
+            }
+            i {
+              position: absolute;
+              top: 50%;
+              right: 0;
+              transform: translateY(-50%);
+            }
+          }
+        }
+        .sub-munes {
+          position: absolute;
+          top: 0;
+          left: 259px;
+          z-index: 2;
+          padding: 10px 20px;
+          width: 310px;
+          height: 182px;
+          background-color: #fff;
+          border: 1px solid #ddd;
+          .sub-munes-item {
+            height: 36px;
+            line-height: 36px;
+            font: 14px 'Source Sans Pro';
+            vertical-align: top;
+            a {
+              color: #ffa500;
+              i {
+                position: relative;
+                top: 5px;
+                height: 32px;
+                font-size: 24px;
+                font-style: oblique;
+              }
+              strong {
+                margin: 0 10px;
+              }
+              span {
+                color: #999;
+              }
+            }
+          }
+        }
+      }
+    }
+    .main {
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 700px;
+      height: 100%;
+      background-color: blue;
+    }
+  }
+}
 </style>
